a10c48ffae6e0d362444f25b4513e279bc5e2bab
StepanIP <scrupnichuk@gmail.com>
2023-07-12 21:43:21 +0300

    Initial commit

---
+++ src/main/java/com/example/emergencynotificationsystem/EmergencyNotificationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.emergencynotificationsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmergencyNotificationSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmergencyNotificationSystemApplication.class, args);
+    }
+
+}
