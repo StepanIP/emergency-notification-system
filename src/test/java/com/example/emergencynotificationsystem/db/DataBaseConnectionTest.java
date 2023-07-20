a8c42316510efe18b1d310ff0f7120978ccb44f5
StepanIP <scrupnichuk@gmail.com>
2023-07-20 21:07:12 +0300

    wrote DataBaseConnectionTests

---
+++ src/test/java/com/example/emergencynotificationsystem/db/DataBaseConnectionTest.java
@@ -0,0 +1,41 @@
+package com.example.emergencynotificationsystem.db;
+
+import com.example.emergencynotificationsystem.model.User;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import com.example.emergencynotificationsystem.repository.UserRepository;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+@DataJpaTest
+@ExtendWith(SpringExtension.class)
+public class DataBaseConnectionTest {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Test
+    @Transactional
+    void testCreateUser(){
+        User user = new User();
+        user.setName("Test");
+        user.setSurname("Test");
+        user.setContact("myGmail@gmail.com");
+
+        int beforeSize = userRepository.findAll().size();
+
+        userRepository.save(user);
+        User actual = userRepository.findUserByContact(user.getContact());
+
+        assertEquals(user.getName(), actual.getName());
+        assertEquals(user.getSurname(), actual.getSurname());
+        assertEquals(user.getContact(), actual.getContact());
+        assertNotEquals(beforeSize, userRepository.findAll().size());
+    }
+
+}
