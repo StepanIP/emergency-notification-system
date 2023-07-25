201f93847f5784bab4590f0d6e1c5237a8166a63
StepanIP <scrupnichuk@gmail.com>
2023-07-25 18:01:37 +0300

    Logging has been added to NotificationServiceImpl

--- src/main/java/com/example/emergencynotificationsystem/service/impl/NotificationServiceImpl.java
+++ src/main/java/com/example/emergencynotificationsystem/service/impl/NotificationServiceImpl.java
@@ -4,56 +4,86 @@ import com.example.emergencynotificationsystem.exception.NullEntityReferenceExce
 import com.example.emergencynotificationsystem.model.Notification;
 import com.example.emergencynotificationsystem.repository.NotificationRepository;
 import com.example.emergencynotificationsystem.service.NotificationService;
-import javax.persistence.EntityNotFoundException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.EntityNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class NotificationServiceImpl implements NotificationService {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(NotificationServiceImpl.class);
+
     @Autowired
     private NotificationRepository notificationRepository;
 
     @Override
-    public Notification create(Notification user) {
-        if (user != null) {
-            return notificationRepository.save(user);
+    public Notification create(Notification notification) {
+        LOGGER.info("Creating notification: {}", notification);
+        if (notification != null) {
+            Notification savedNotification = notificationRepository.save(notification);
+            LOGGER.info("Notification created successfully.");
+            return savedNotification;
         }
+        LOGGER.error("Notification cannot be 'null'");
         throw new NullEntityReferenceException("Notification cannot be 'null'");
     }
 
     @Override
     public Notification readById(long id) {
+        LOGGER.info("Fetching notification by ID: {}", id);
         return notificationRepository.findById(id).orElseThrow(
-                () -> new EntityNotFoundException("Notification with id " + id + " not found"));
+                () -> {
+                    LOGGER.error("Notification with ID {} not found", id);
+                    return new EntityNotFoundException("Notification with id " + id + " not found");
+                }
+        );
     }
 
     @Override
-    public Notification update(Notification user) {
-        if (user != null) {
-            readById(user.getId());
-            return notificationRepository.save(user);
+    public Notification update(Notification notification) {
+        LOGGER.info("Updating notification: {}", notification);
+        if (notification != null) {
+            readById(notification.getId());
+            Notification updatedNotification = notificationRepository.save(notification);
+            LOGGER.info("Notification updated successfully.");
+            return updatedNotification;
         }
+        LOGGER.error("Notification cannot be 'null'");
         throw new NullEntityReferenceException("Notification cannot be 'null'");
     }
 
     @Override
-    public void delete(Notification user) {
-        notificationRepository.delete(user);
+    public void delete(Notification notification) {
+        LOGGER.info("Deleting notification: {}", notification);
+        notificationRepository.delete(notification);
+        LOGGER.info("Notification deleted successfully.");
     }
 
     @Override
     public List<Notification> getAll() {
-        List<Notification> users = notificationRepository.findAll();
-        return users.isEmpty() ? new ArrayList<>() : users;
+        LOGGER.info("Fetching all notifications.");
+        List<Notification> notifications = notificationRepository.findAll();
+        if (notifications.isEmpty()) {
+            LOGGER.info("No notifications found.");
+            return new ArrayList<>();
+        }
+        LOGGER.info("Fetched {} notifications.", notifications.size());
+        return notifications;
     }
 
     @Override
     public Notification readByName(String name) {
+        LOGGER.info("Fetching notification by name: {}", name);
         return notificationRepository.findUserByName(name).orElseThrow(
-                () -> new EntityNotFoundException("Notification with name " + name + " not found"));
+                () -> {
+                    LOGGER.error("Notification with name {} not found", name);
+                    return new EntityNotFoundException("Notification with name " + name + " not found");
+                }
+        );
     }
 }
