ddca16252fab630ca0290a063a8a1889513f97fa
StepanIP <scrupnichuk@gmail.com>
2023-07-25 18:10:35 +0300

    Logging has been added to EmailSenderServiceImpl

--- src/main/java/com/example/emergencynotificationsystem/service/messageSending/email/impl/EmailSenderServiceImpl.java
+++ src/main/java/com/example/emergencynotificationsystem/service/messageSending/email/impl/EmailSenderServiceImpl.java
@@ -2,6 +2,8 @@ package com.example.emergencynotificationsystem.service.messageSending.email.imp
 
 import com.example.emergencynotificationsystem.configuration.email.MailConfiguration;
 import com.example.emergencynotificationsystem.service.messageSending.email.EmailSenderService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
@@ -9,8 +11,9 @@ import org.springframework.stereotype.Service;
 @Service
 public class EmailSenderServiceImpl implements EmailSenderService {
 
-    private final JavaMailSender mailSender;
+    private static final Logger LOGGER = LoggerFactory.getLogger(EmailSenderServiceImpl.class);
 
+    private final JavaMailSender mailSender;
     private final MailConfiguration mailConfiguration;
 
     public EmailSenderServiceImpl(JavaMailSender mailSender, MailConfiguration mailConfiguration) {
@@ -28,9 +31,13 @@ public class EmailSenderServiceImpl implements EmailSenderService {
 
     @Override
     public void sendEmail(String to, String subject, String message) {
-        if(mailConfiguration.getUsername()==null){
-            throw new NullPointerException("Error, Sender can not be null");
+        LOGGER.info("Sending email to: {}, subject: {}", to, subject);
+
+        if (mailConfiguration.getUsername() == null) {
+            LOGGER.error("Error, Sender cannot be null");
+            throw new NullPointerException("Error, Sender cannot be null");
         }
+
         SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
         simpleMailMessage.setFrom(mailConfiguration.getUsername());
         simpleMailMessage.setTo(to);
@@ -38,5 +45,7 @@ public class EmailSenderServiceImpl implements EmailSenderService {
         simpleMailMessage.setText(message);
 
         this.mailSender.send(simpleMailMessage);
+
+        LOGGER.info("Email sent successfully.");
     }
 }
