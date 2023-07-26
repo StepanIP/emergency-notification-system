9ca224631ee63d7bbb5b9deff625d77e4f870826
StepanIP <scrupnichuk@gmail.com>
2023-07-26 22:44:12 +0300

    Created AuthTokenFilter and UserDetailsService

---
+++ src/main/java/com/example/emergencynotificationsystem/service/security/UserDetailsServiceImpl.java
@@ -0,0 +1,21 @@
+package com.example.emergencynotificationsystem.service.security;
+
+import com.example.emergencynotificationsystem.model.User;
+import com.example.emergencynotificationsystem.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(username);
+        return UserDetailsImpl.build(user);
+    }
+}
---
+++ src/main/java/com/example/emergencynotificationsystem/service/security/jwt/AuthTokenFilter.java
@@ -0,0 +1,70 @@
+package com.example.emergencynotificationsystem.service.security.jwt;
+
+import com.example.emergencynotificationsystem.configuration.jwt.JwtUtils;
+import com.example.emergencynotificationsystem.service.security.UserDetailsServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.util.ObjectUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class AuthTokenFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private JwtUtils jwtUtils;
+    @Autowired
+    private UserDetailsServiceImpl userDetailsService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse,
+                                    FilterChain filterChain)
+            throws ServletException, IOException {
+        if (!hasAuthorizationBearer(httpServletRequest)) {
+            filterChain.doFilter(httpServletRequest, httpServletResponse);
+            return;
+        }
+        String token = getAccessToken(httpServletRequest);
+        if (!jwtUtils.validateJwtToken(token)) {
+            filterChain.doFilter(httpServletRequest, httpServletResponse);
+            return;
+        }
+        setAuthenticationContext(token, httpServletRequest);
+        filterChain.doFilter(httpServletRequest, httpServletResponse);
+    }
+
+    private void setAuthenticationContext(String token, HttpServletRequest request) {
+        UserDetails userDetails = getUserDetails(token);
+        UsernamePasswordAuthenticationToken
+                authentication = new UsernamePasswordAuthenticationToken(userDetails.getUsername(),
+                userDetails.getPassword(), userDetails.getAuthorities());
+        authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+    }
+
+    private UserDetails getUserDetails(String token) {
+        String[] jwtSubject = jwtUtils.getSubject(token).split(",");
+        return userDetailsService.loadUserByUsername(jwtSubject[0]);
+    }
+
+    private boolean hasAuthorizationBearer(HttpServletRequest request) {
+        String header = request.getHeader("Authorization");
+        if (ObjectUtils.isEmpty(header) || !header.startsWith("Bearer")) {
+            return false;
+        }
+        return true;
+    }
+
+    private String getAccessToken(HttpServletRequest request) {
+        String header = request.getHeader("Authorization");
+        return header.split(" ")[1].trim();
+    }
+
+}
